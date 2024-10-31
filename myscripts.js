8faa7ffc2b5e59a988688d0d9482c3d66ca47de5
Phil5184 <p3mai@ucsd.edu>
2024-10-31 16:51:43 -0700

    worked on website

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function side_barClose() {
+    document.getElementById("sidebar").style.display = 'none';
+}
+
+function side_barOpen() {
+    document.getElementById('sidebar').style.display = 'block';
+}
